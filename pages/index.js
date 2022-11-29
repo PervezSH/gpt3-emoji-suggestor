8f43afb0480e3e31d9faf77107c62a04704b7a77
PervezSH <50441829+PervezSH@users.noreply.github.com>
2022-11-29 15:50:39 +0530

    feat: initialize project 🎉

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,15 @@ const Home = () => {
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>GPT-3 Emoji | Enhance</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>🤖 emoji, enhance</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>generate emojis to accurately represent users' feelings on social media</h2>
           </div>
         </div>
       </div>
